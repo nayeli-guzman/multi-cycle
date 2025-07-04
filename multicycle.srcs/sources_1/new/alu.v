b46ec0920ac4341d2a6da8104a1871b0b243a0b7
ReatorG <166132244+ReatorG@users.noreply.github.com>
2025-07-04 12:15:42 -0500

    add: div

--- multicycle.srcs/sources_1/new/alu.v
+++ multicycle.srcs/sources_1/new/alu.v
@@ -2,7 +2,7 @@
 
 module alu (
   input [31:0] a, b,
-  input [2:0] ALUControl,
+  input [3:0] ALUControl,
   output reg [31:0] Result,
   output wire [3:0] ALUFlags,
   output reg [31:0] ALUMulti
@@ -18,17 +18,19 @@ reg[63:0] multiplication;
 
 always @(*) begin
   casex (ALUControl)
-    3'b00?: Result = sum;
-    3'b010: Result = a & b;
-    3'b011: Result = a | b;
-    3'b100: Result = a << b;
-    3'b101: Result = a * b;
-    3'b110: begin  // UMUL
+    4'b000?: Result = sum;
+    4'b0010: Result = a & b;
+    4'b0011: Result = a | b;
+    4'b0100: Result = a << b;
+    4'b0101: Result = a * b;
+    4'b1000: Result = b;
+    4'b1001: Result = a / b; // DIV
+    4'b0110: begin  // UMUL
         multiplication = $unsigned(a) * $unsigned(b);
         ALUMulti = multiplication[31:0];
         Result = multiplication[63:32];
       end
-      3'b111: begin  // SMUL
+      4'b0111: begin  // SMUL
         multiplication = $signed(a) * $signed(b);
         ALUMulti = multiplication[31:0];
         Result = multiplication[63:32];
